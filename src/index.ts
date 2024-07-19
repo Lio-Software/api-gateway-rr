d057fd3348189c9b31c28ffb4a5693d7ad63940f
Mauricio Cruz <mawcp7@gmail.com>
2024-07-18 23:27:12 -0600

    chore: Add dependencies for multer and update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,44 @@
-import express, { Application } from "express";
-import morgan from "morgan";
-
+import express from "express";
 import dotenv from "dotenv";
 import { Signale } from "signale";
+import morgan from "morgan";
+import { } from "../tsconfig.json";
+import cors from "cors";
 import proxy from "express-http-proxy";
 
-const app: Application = express();
+const app = express();
 const signale = new Signale();
 
 dotenv.config();
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
+
 const PORT = process.env.PORT || 3000;
 const API_PREFIX = process.env.API_PREFIX || "/api/v1";
 
+app.options("*", cors())
+app.use(cors());
+
 const AUTH_SERVICE_URL = process.env.AUTH_SERVICE_URL || "http://localhost:3001";
+const USER_SERVICE_URL = process.env.USER_SERVICE_URL || "http://localhost:3002";
+const VEHICLE_SERVICE_URL = process.env.VEHICLE_SERVICE_URL || "http://localhost:3003";
 
 app.use(`${API_PREFIX}/auth`, proxy(`${AUTH_SERVICE_URL}/api/v1/auth`, {
     proxyReqPathResolver: (req) => {
         return `/api/v1/auth${req.url}`;
+    }
+}));
+
+app.use(`${API_PREFIX}/users`, proxy(`${USER_SERVICE_URL}/api/v1/users`, {
+    proxyReqPathResolver: (req) => {
+        return `/api/v1/users${req.url}`;
+    }
+}));
+
+app.use(`${API_PREFIX}/vehicles`, proxy(`${VEHICLE_SERVICE_URL}/api/v1/vehicles`, {
+    proxyReqPathResolver: (req) => {
+        return `/api/v1/vehicles${req.url}`;
     }
 }));
 
